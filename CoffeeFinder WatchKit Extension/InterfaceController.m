e4a4d5a77c5427b16171b2781338fd3c3229d935
David Olesch <david@jackrabbitmobile.com>
2014-11-26 17:43:06 -0600

    Implemented map in the watch app.

--- CoffeeFinder WatchKit Extension/InterfaceController.m
+++ CoffeeFinder WatchKit Extension/InterfaceController.m
@@ -11,7 +11,9 @@
 
 @interface InterfaceController()
 
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *headerLabel;
 @property (weak, nonatomic) IBOutlet WKInterfaceLabel *locationDescriptionLabel;
+@property (weak, nonatomic) IBOutlet WKInterfaceMap *locationMap;
 @property (weak, nonatomic) IBOutlet WKInterfaceButton *locateNearbyCoffeeButton;
 
 @end
@@ -26,7 +28,7 @@
         // Configure interface objects here.
         NSLog(@"%@ initWithContext", self);
         [self.locationDescriptionLabel setText:@""];
-        
+        [self.locationMap setHidden:YES];
     }
     return self;
 }
@@ -45,10 +47,18 @@
 - (void)locateNearbyCoffee
 {
     [self.locationDescriptionLabel setText:@"Loading.."];
+    [self.locationMap setHidden:YES];
+    [self.headerLabel setHidden:YES];
+    [self.locationMap removeAllAnnotations];
     [self.locateNearbyCoffeeButton setEnabled:NO];
     [CoffeeFinder findCoffeeNear:CLLocationCoordinate2DMake(userLatitude, userLongitude) withCompletionBlock:^(MKMapItem *coffeeItem) {
         [self.locationDescriptionLabel setText:coffeeItem.name];
         [self.locateNearbyCoffeeButton setEnabled:YES];
+        [self.locationMap setHidden:NO];
+        [self.locationMap addAnnotation:coffeeItem.placemark.location.coordinate withPinColor:WKInterfaceMapPinColorRed];
+        
+        MKCoordinateRegion region = MKCoordinateRegionMake(coffeeItem.placemark.location.coordinate, MKCoordinateSpanMake(0.005f, 0.005f));
+        [self.locationMap setCoordinateRegion:region];
     }];
 }
 
